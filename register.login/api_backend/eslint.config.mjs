c5fc3f9e680143a967f6e4a80f4dca4817216979
Monique Jackson <ST10096757@vcconnect.edu.za>
2024-10-07 21:56:12 +0200

    More work on the ESLint config

--- register.login/api_backend/eslint.config.mjs
+++ register.login/api_backend/eslint.config.mjs
@@ -8,12 +8,17 @@ export default [
     languageOptions: {
       globals: {
         ...globals.browser,
-        require: "readonly", // Add require as a global
-        process: "readonly", // If you use process
+        require: "readonly", // Allow 'require' as a global
+        process: "readonly", // Allow 'process' as a global
       },
       parserOptions: {
-        ecmaVersion: 12,
+        ecmaVersion: 12, // Use ECMAScript 2021
         sourceType: "script", // Allow CommonJS syntax
+      },
+      settings: {
+        react: {
+          version: "detect", // Automatically detect React version
+        },
       },
     },
   },
