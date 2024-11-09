e4a4690a31977fb2096875487aa3d2e5980de009
ST10112350 <ST10112350@vcconnect.edu.za>
2024-11-09 20:18:28 +0200

    frontend and backend vali code

--- register.login/Backend/src/routes/auth.js
+++ register.login/Backend/src/routes/auth.js
@@ -3,12 +3,25 @@ import bcrypt from 'bcryptjs';
 import User from '../models/user.js';
 import expressBrute from 'express-brute';
 import bruteforce from '../middleware/bruteForce.js';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-// Register Route
-router.post('/register', bruteforce.prevent, async (req, res) => {
-    const { username, password, userFirstName, userLastName, idNumber, accountNumber, isEmployee } = req.body;
+// Register Route with validation
+router.post('/register', [
+    bruteforce.prevent,
+    body('username').matches(/^[a-zA-Z0-9]{3,20}$/).withMessage('Username must be 3-20 alphanumeric characters'),
+    body('password').isLength({ min: 6 }).matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/).withMessage('Password must be at least 6 characters, including at least one letter and one number'),
+    body('idNumber').matches(/^[0-9]{13}$/).withMessage('ID Number must be a 13-digit number'),
+    body('accountNumber').matches(/^[0-9]{10}$/).withMessage('Account Number must be a 10-digit number'),
+    
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password, userFirstName, userLastName, idNumber, accountNumber } = req.body;
     try {
         const existingUser = await User.findOne({ username });
         if (existingUser) {
@@ -16,7 +29,7 @@ router.post('/register', bruteforce.prevent, async (req, res) => {
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ username, userFirstName, userLastName, password: hashedPassword, idNumber, accountNumber, isEmployee });
+        const newUser = new User({ username, userFirstName, userLastName, password: hashedPassword, idNumber, accountNumber });
         
         await newUser.save();
         res.status(201).json({ message: 'User registered successfully' });
@@ -26,8 +39,17 @@ router.post('/register', bruteforce.prevent, async (req, res) => {
     }
 });
 
-// Login Route
-router.post('/login', bruteforce.prevent, async (req, res) => {
+// Login Route wuth validation
+router.post('/login', [
+    bruteforce.prevent,
+    body('username').notEmpty().withMessage('Username is required'),
+    body('password').notEmpty().withMessage('Password is required'),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
     const { username, password, isEmployee } = req.body;
 
     const user = await User.findOne({ username });
